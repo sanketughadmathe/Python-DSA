97ffcf9923b4a99c1093318d0aa040dc6a45505b
sanketughadmathe <sanketughadmathe@gmail.com>
2023-07-29 21:22:54 +0530

    New File

---
+++ 04. Trees/01. Trees Constructor.py
@@ -0,0 +1,58 @@
+class Node:
+    def __init__(self, val=None):
+        self.value = val
+        self.left_child = None
+        self.right_child = None
+
+
+class BinartSearchTree:
+    # Constructor for creating a new BST object with root node as the given value
+    def __init__(self):
+        self.root = None
+
+    def insert(self, value):
+        newNode = Node(value)
+        if self.root == None:
+            self.root = newNode
+            return True
+        current = self.root
+        while (True):
+            if newNode.value == current.value:
+                return False
+            if newNode.value < current.value:
+                if current.left_child == None:
+                    current.left_child = newNode
+                    return True
+                current = current.left_child
+            else:
+                if current.right_child == None:
+                    current.right_child = newNode
+                    return True
+                current = current.right_child
+
+    def contains(self, value):
+        current = self.root
+        while (current != None):
+            if value < current.value:
+                current = current.left_child
+            elif value > current.value:
+                current = current.right_child
+            else:  # found it!
+                return True
+        return False
+
+
+my_tree = BinartSearchTree()
+my_tree.insert(47)
+my_tree.insert(21)
+my_tree.insert(76)
+my_tree.insert(18)
+my_tree.insert(27)
+my_tree.insert(52)
+my_tree.insert(82)
+my_tree.insert(1)
+my_tree.insert(3)
+
+print(my_tree.contains(17))
+# print(my_tree.root.left_child.value)
+# print(my_tree.root.right_child.value)
